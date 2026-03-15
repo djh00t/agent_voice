ada2803ee5a588534b83fdb1875e57be1571fe8e
David Hooton <david@hooton.org>
2026-03-15 22:49:00 +1100

    docs(agent-voice): add docs sidebar configuration

---
+++ website/sidebars.js
@@ -0,0 +1,12 @@
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+module.exports = {
+  docs: [
+    'overview',
+    'configuration',
+    'control-api',
+    'address-book',
+    'deployment',
+    'testing',
+    'release'
+  ]
+};
